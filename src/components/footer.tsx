44ded9e4a094bd42a78c52b8018a20c4c8aed1d2
Arpit Pareek <arpitpareek051@gmail.com>
2024-03-14 17:34:25 +0530

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,52 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer pt-3  ">
+        <div className="row align-items-center justify-content-lg-between">
+          <div className="col-lg-6">
+            <ul className="nav nav-footer justify-content-center justify-content-lg-end">
+              <li className="nav-item">
+                <a
+                  href="https://www.creative-tim.com"
+                  className="nav-link text-sm text-muted"
+                  target="_blank"
+                >
+                   
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="https://www.creative-tim.com/presentation"
+                  className="nav-link text-sm text-muted"
+                  target="_blank"
+                >
+                  About Us
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="https://www.creative-tim.com/blog"
+                  className="nav-link text-sm text-muted"
+                  target="_blank"
+                >
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="https://www.creative-tim.com/license"
+                  className="nav-link text-sm pe-0 text-muted"
+                  target="_blank"
+                >
+                  License
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+
